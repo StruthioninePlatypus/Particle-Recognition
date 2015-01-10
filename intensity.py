228ac7243def3a0df500fa9953171ca879ef923e
StruthioninePlatypus <cheesegrater@me.com>
2015-01-10 19:09:06 +0000

    Create intensity.py

---
+++ intensity.py
@@ -0,0 +1,36 @@
+from mech import *
+
+def maxEnPt(pts):
+    # Returns point(s) with highest intensity
+    big = 0
+    bigs = []
+    for i in pts:
+        if i[2]>big: big = i[2]
+    for i in pts:
+        if i[2]==big: bigs.append(i)
+    return bigs
+
+def weightedCentroid(pts):
+    # Centroid weighted with intensity (uses centre of mass formula)
+    x,y = split(pts)
+    M,s = 0, [0,0]
+    for i in range(len(pts)):
+        M += pts[i][2]
+        s[0] += pts[i][0]*pts[i][2]
+        s[1] += pts[i][1]*pts[i][2]
+    return ((1/M)*s[0],(1/M)*s[1])
+    
+def variation(pts):
+    # Returns variation in intensity
+    anyp,var = pts[0],0.0
+    start = anyp[2]
+    for i in range(1,len(pts)):
+        var += abs(start-pts[i][2])
+    return var
+    
+def meanEn(pts):
+    # Returns average intensity of all points
+    tot = 0
+    for i in pts:
+        tot += i[2]
+    return tot/len(pts)
