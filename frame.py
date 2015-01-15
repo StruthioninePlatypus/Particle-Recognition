5caf3b900fe2edb12169d219d333774d39db6c67
StruthioninePlatypus <cheesegrater@me.com>
2015-01-15 13:15:17 +0000

    Update frame.py

--- frame.py
+++ frame.py
@@ -1,5 +1,3 @@
-# class for metadata, needs work...    
-
 from impact import *
 
 class frame:
@@ -18,23 +16,36 @@ class frame:
         
     def frequency(self):
         # Crude frequency data - could be used for checking algorithm with Gaussian elimination later
-        p,b,a,g,h = 0,0,0,0,0
+        p,b,a,g,h = 0.0,0.0,0.0,0.0,0.0
         dat = self.classify()
         for i in dat.keys():
             print dat[i]
-            if dat[i] == 'proton': p += 1
-            if dat[i] == 'alpha': a += 1
-            if dat[i] == 'gamma': g += 1
-            if dat[i] == 'beta': b += 1
-            if dat[i] == 'heavy ion': h += 1
-        frtot = len(self.impacts)
+            if dat[i] == 'proton': p += 1.0
+            if dat[i] == 'alpha': a += 1.0
+            if dat[i] == 'gamma': g += 1.0
+            if dat[i] == 'beta': b += 1.0
+            if dat[i] == 'heavy ion': h += 1.0
+        print p,a,g,b,h
+        frtot = float(len(self.impacts))
         print 'There are', frtot, 'blobs in the frame'
-        print round(g/frtot,2), 'are gamma particles'
-        print round(h/frtot,2), 'are heavy ions'
-        print round(p/frtot,2), 'are protons'
-        print round(b/frtot,2), 'are beta particles'
-        print round(a/frtot,2), 'are alpha particles'
+        print round(g/frtot,2)*100, '% are gamma particles'
+        print round(h/frtot,2)*100, '% are heavy ions'
+        print round(p/frtot,2)*100, '% are protons'
+        print round(b/frtot,2)*100, '% are beta particles'
+        print round(a/frtot,2)*100, '% are alpha particles'
 
     def display(self):
         for i in self.impacts:
             i.showthyself()
+
+def filereader(f):
+    f,final = open(f),[]
+    while True:
+        try:
+            b = eval(f.readline())
+            final.append(b)
+        except:
+            break
+    return final
+f =  frame(filereader('H:/vals.txt'))
+f.frequency()
